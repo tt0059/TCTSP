faa8b624583296c976a34152f82df79d336b6967
tangt <54666012+tt0059@users.noreply.github.com>
2023-02-15 09:28:02 +0800

    Update train.sh

--- experiments/Xlan_SAP_V6_kmeans_wt03_RL_wt05_CIDEr_25_test/train.sh
+++ experiments/Xlan_SAP_V6_kmeans_wt03_RL_wt05_CIDEr_25_test/train.sh
@@ -1,7 +1,6 @@
 CUDA_VISIBLE_DEVICES=6 \
 python \
-/home/tangt/nfs_tangt/code/TCTSP/main_test.py \
---folder /home/tangt/nfs_tangt/code/TCTSP/experiments/Xlan_SAP_V6_kmeans_wt03_RL_wt05_CIDEr_25_test \
+main_test.py \
+--folder ./experiments/Xlan_SAP_V6_kmeans_wt03_RL_wt05_CIDEr_25_test \
 --resume 57 \
---markov_mat_path /home/tangt/nfs_tangt/code/TCTSP/data/markov_mat_kmeans.npy \
---topic_num 82 \
+--markov_mat_path ./data/markov_mat_kmeans.npy \
