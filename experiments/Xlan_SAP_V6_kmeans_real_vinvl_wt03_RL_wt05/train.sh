7ade6ec8e3d72cbd27a6dd5d4e0f04d775dc9b73
tangt <54666012+tt0059@users.noreply.github.com>
2025-03-05 14:21:32 +0800

    Add files via upload

---
+++ experiments/Xlan_SAP_V6_kmeans_real_vinvl_wt03_RL_wt05/train.sh
@@ -0,0 +1,10 @@
+CUDA_VISIBLE_DEVICES=1 \
+python -m torch.distributed.launch \
+--nproc_per_node=1 \
+/home/tangt/my-image-to-paragraph/main_soft_tt_VinVL.py \
+--folder /home/tangt/my-image-to-paragraph/experiments/Xlan_SAP_V6_kmeans_real_vinvl_wt03_RL_wt05/ \
+--resume 0 \
+--topic_weight 0.05 \
+--tensorboard_path /home/tangt/my-image-to-paragraph/experiments/Xlan_SAP_V6_kmeans_real_vinvl_wt03_RL_wt05/log \
+--markov_mat_path /nfs/tangt/code/my-image-to-paragraph/data_vg/markov_mat_kmeans.npy \
+--topic_num 82 \
